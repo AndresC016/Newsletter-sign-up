acca94573e8442956b655fb0a9168a6c4414c17b
Andres Cerquera <acerquera7v7@gmail.com>
2024-01-23 02:31:08 -0500

    Implementacion de css grid para mobile y desktop

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const form = document.querySelector('form');
 const singUp = document.querySelector('.sing-up-container');
 const successfulCard = document.querySelector('.successful-card');
 
+const validateEmail = () => {
+    const emailInput = document.getElementById('email');
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     singUp.classList.add('dissapear');
