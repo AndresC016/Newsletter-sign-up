07aae051a906f7b626312098f39d755b0272826c
Andres Cerquera <acerquera7v7@gmail.com>
2024-01-23 14:47:33 -0500

    Actualizacion de estados con js y conclusion del proyecto

--- script.js
+++ script.js
@@ -4,10 +4,34 @@ const successfulCard = document.querySelector('.successful-card');
 
 const validateEmail = () => {
     const emailInput = document.getElementById('email');
+    const error = document.querySelector('.error');
+    const emailEntered = document.getElementById('email-entered');
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    
+
+    if(regex.test(emailInput.value)) {
+        error.textContent = '';
+        emailEntered.textContent = emailInput.value;
+        emailInput.style.border = '1px solid hsl(234, 29%, 20%)';
+    } else {
+        error.textContent = 'Valid email required';
+        emailInput.style.background = 'hsl(4, 100%, 95%)';
+        emailInput.style.border = '1px solid hsl(4, 100%, 67%)';
+        emailInput.style.color = 'hsl(4, 100%, 67%)';
+    }
 }
 
 form.addEventListener('submit', (e) => {
+    const buttonEmail = document.getElementById('button-email');
+    
     e.preventDefault();
     singUp.classList.add('dissapear');
     successfulCard.classList.remove('dissapear');
+
+    if(emailInput.value.trim() === '') {
+        buttonEmail.disabled = true;
+    } else {
+        buttonEmail.disabled = false;
+    }
 })
